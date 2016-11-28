46f06b8a641fc2b37ec58636ebcf62e323cf147f
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2016-11-28 13:34:36 +0100

    Now uses arduino from PATH to compile

--- arduino/verify.sh
+++ arduino/verify.sh
@@ -8,8 +8,7 @@ cp -a ../../src/{src.ino,src.h} .
 cp -a ../../api/* .
 cp -a ../../lib/SparkFun_ISL29125_Breakout_Arduino_Library/src/* .
 
-#Arduino is the dir in which the arduino binary is located
-../../../../arduino/arduino --verify src.ino
+arduino --verify src.ino
 
 cd ../..
 rm -rf build
